d3a6f9d546d36e6b8e028ca15d2138285013ed86
Kevin Lin <kevin.lin.p@gmail.com>
2022-05-04 22:32:41 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const http = require('http')
+const { execSync } = require("child_process");
+
+http.createServer(function (req, res) {
+  try {
+    const version = execSync("emcc --version").toString()
+    res.write(version)
+  } catch (error) {
+    console.log(error)
+    res.write('error')
+  }
+
+  res.end(); 
+}).listen(8080); 
